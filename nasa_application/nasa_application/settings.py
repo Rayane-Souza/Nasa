04208230d0bf28518384e8cbd8cd9ad3a295f7d9
Bearodrigues <bealimaa.rodrigues@gmail.com>
2024-12-06 12:42:36 -0300

    ajuste config

--- nasa_application/nasa_application/settings.py
+++ nasa_application/nasa_application/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-^*+@4tw+59ai5(*m5dvmr#k*bxx^px=i(e@$as*uk!k5y#brc9
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
