5fffb92057d3509ce447c5fe81e2c0a7b15c78cd
Bearodrigues <bealimaa.rodrigues@gmail.com>
2024-12-07 11:36:37 -0300

    ajuste teste deploy

--- nasa_application/nasa_application/wsgi.py
+++ nasa_application/nasa_application/wsgi.py
@@ -7,17 +7,10 @@ For more information on this file, see
 https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 """
 
-"""
-WSGI config for nasa_application project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/stable/howto/deployment/wsgi/
-"""
-
 import os
+
 from django.core.wsgi import get_wsgi_application
+
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'nasa_application.settings')
 
