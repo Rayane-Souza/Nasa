c0b1ba2f650c4cf848fb7a23733eb4f6ffcb009e
Bearodrigues <bealimaa.rodrigues@gmail.com>
2024-12-08 11:53:25 -0300

    deploy

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn nasa_application.wsgi:application --bind 0.0.0.0:$PORT
+web: gunicorn nasa_application.wsgi:application
--- nasa_application/nasa_application/settings.py
+++ nasa_application/nasa_application/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -143,3 +144,5 @@ STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATICFILES_DIRS = [BASE_DIR / "static"]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
